d6ae3f23b9e0d2dc781cd32cf855117fc4815f1c
Mike Petersen <mike@odania-it.de>
2017-10-09 20:21:33 +0200

    Updated AWS Helper

--- lib/odania_ops/helper/aws.rb
+++ lib/odania_ops/helper/aws.rb
@@ -5,16 +5,21 @@ module OdaniaOps
 		module Aws
 			class << self
 				def configure
-					Aws.config.update(
+					::Aws.config.update(
 						{
 							region: $config['aws']['region'],
-							credentials: Aws::Credentials.new($config['aws']['access_key_id'], $config['aws']['secret_access_key'])
+							credentials: ::Aws::Credentials.new($config['aws']['access_key_id'], $config['aws']['secret_access_key'])
 						}
 					)
 				end
 
 				def tags(repository_name)
-					client = Aws::ECR::Client.new
+					client = ::Aws::ECR::Client.new(
+							{
+									region: $config['aws']['region'],
+									credentials: ::Aws::Credentials.new($config['aws']['access_key_id'], $config['aws']['secret_access_key'])
+							}
+					)
 					result = []
 					response = client.describe_images(repository_name: repository_name)
 					response.image_details.each do |aws_image_details|
